fc6f0f96666411f5cf8f8d64fd0759622d0d30b6
nthreem <nthreem@gmail.com>
2018-02-09 10:41:52 -0500

    Update permaban.py

--- minqlx-plugins/permaban.py
+++ minqlx-plugins/permaban.py
@@ -10,6 +10,7 @@ class permaban(minqlx.Plugin):
         self.add_hook("player_connect", self.handle_player_connect, priority=minqlx.PRI_HIGH)
 
         self.add_command("permaban", self.cmd_permaban, 5, usage="<id>/<steam_id>")
+        self.add_command("unpermaban", self.cmd_unpermaban, 5, usage="<id>/<steam_id>")
         self.add_command("tomtec_versions", self.cmd_showversion)
 
         self.plugin_version = "1.5"
@@ -43,6 +44,32 @@ class permaban(minqlx.Plugin):
 
         channel.reply("{}^7 has been permanently banned.".format(player_name))
         
+    def cmd_unpermaban(self, player, msg, channel):
+        if len(msg) < 2:
+            return minqlx.RET_USAGE
+        
+        try:
+            ident = int(msg[1])
+            target_player = None
+            if 0 <= ident < 64:
+                steam_id = self.player(int(msg[1])).steam_id
+                player_name = self.player(int(msg[1])).name
+            else:
+                player_name = ident
+                steam_id = ident
+        except ValueError:
+            channel.reply("Invalid ID. Use either a client ID or a SteamID64.")
+            return
+        except minqlx.NonexistentPlayerError:
+            channel.reply("Invalid client ID. Use either a client ID or a SteamID64.")
+            return
+
+        self.db.set("minqlx:players:{}:permabanned".format(steam_id), "0")
+
+        if self.player(steam_id):
+            self.kick(self.player(steam_id), "You have been unbanned.")
+
+        channel.reply("{}^7 has been unbanned.".format(player_name))
         
     def handle_player_connect(self, player):
         is_banned_id = self.is_banned(player)
